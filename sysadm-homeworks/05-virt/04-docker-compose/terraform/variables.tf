4d4bb355202f6c90422e5a5c038b3b930686e620
Aleksei Shtepa <a@shtepa.net>
2022-05-06 15:00:15 +0300

    05-virt-04-docker-compose

---
+++ sysadm-homeworks/05-virt/04-docker-compose/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gaaaddh3mpmif8gr7e"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g3p7bhu478vd7p42qa"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8rd1s8oum678euff1r"
+}
