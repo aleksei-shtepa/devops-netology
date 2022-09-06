7be166164d905125e84ccc91bb3baf69e2abf371
Aleksei Shtepa <a@shtepa.net>
2022-09-06 17:29:22 +0300

    WIP 09-ci-04-jenkins

---
+++ mnt-homework/09-ci/04-jenkins/infrastructure/terraform/yandex/main.tf
@@ -0,0 +1,29 @@
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.yc_region
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network82"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet82"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "instance" {
+  source = "../modules/instance"
+  
+  for_each = toset(["jenkins-master", "jenkins-agent"])
+
+  name = each.key
+  image = "centos-7"
+  memory = 4
+  subnet_id = yandex_vpc_subnet.subnet-1.id
+  nat = true
+}
